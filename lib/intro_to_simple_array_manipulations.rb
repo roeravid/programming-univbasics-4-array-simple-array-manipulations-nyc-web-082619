501a41157aa9300ae40864d816a9329bddce66c9
Joe David <javid92@gmail.com>
2019-08-16 12:08:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,8 @@ colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
     next_color = "violet"
 using_push(colors_in_the_rainbow, next_color)
 
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
 using_unshift(bouroughs_in_nyc,new_neighborhood)
 
 
