9e623543ea9b56af031e258aa0ea1278084ed52f
Joe David <javid92@gmail.com>
2019-08-16 12:09:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,40 @@
+
+using_push(colors_in_the_rainbow, next_color)
+
+
+using_unshift(bouroughs_in_nyc,new_neighborhood)
+
+
+using_pop(continents)
+
+
+pop_with_args(small_dogs)
+
+
+
+using_shift(my_favorite_cities)
+
+
+
+shift_with_args(ice_cream_brands)
+
+
+
+using_concat(my_favorite_things,all_my_favs)
+
+
+
+using_insert(list_of_programming_languages, "Python")
+
+
+using_uniq(haircuts)
+
+
+using_flatten(instruments)
+
+
+using_delete(instructors, "Steven")
+
+
+using_delete_at(famous_robots, 2)
+
